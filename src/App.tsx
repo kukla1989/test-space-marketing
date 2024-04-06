a060c3d88f985d10303f97a4f495e90122a0d710
kukla1989 <kukla.1989@ukr.net>
2024-04-06 19:49:37 +0300

    add article

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,14 @@ import './styles/App.scss';
 import { Navigate, Route, Routes } from 'react-router-dom';
 import { Main } from './pages/Main/Main';
 import { NotFound } from './pages/NotFound/NotFound';
+import HeaderTop from './components/HeaderTop/HeaderTop';
+import Header from './components/Header/Header';
 
 export const App: React.FC = () => {
   return (
     <div className="app">
+      <HeaderTop />
+      <Header />
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/home" element={<Navigate to="/" />} />
