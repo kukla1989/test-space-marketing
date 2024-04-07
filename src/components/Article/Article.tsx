71b9ae07868606016941800c24fb248c57540e10
kukla1989 <kukla.1989@ukr.net>
2024-04-07 10:22:19 +0300

    add form

---
+++ src/components/Article/Article.tsx
@@ -0,0 +1,78 @@
+/* eslint-disable react/no-unescaped-entities */
+/* eslint-disable max-len */
+import React from 'react';
+
+import './article.scss';
+
+const Article = () => {
+  return (
+    <div>
+      <h1>Digital Real</h1>
+      <div className="article">
+        Os portugueses nem acreditam que têm a oportunidade de ganhar dinheiro tão facilmente.
+
+        A nova criptomoeda, Digital Real, traz lucro para as pessoas porque os algoritmos da plataforma que negocia com a nova criptomoeda são baseados em inteligência artificial.
+
+        A invenção desta nova criptomoeda mudou a vida dos cidadãos portugueses.
+
+        O Banco do Brasil S.A., um banco confiável e confiável, impulsionou o projeto. O banco assume toda a responsabilidade pelos fundos dos usuários e usa criptografia avançada e tecnologia segura para proteger as informações financeiras dos usuários.
+        Com 100% de probabilidade de obter lucro com seu investimento no Digital Real, os usuários podem investir com confiança. O Banco do Brasil S.A. visa melhorar a qualidade de vida das pessoas comuns que possam ter sofrido com a crise.
+
+        A plataforma de negociação provou ser um sucesso, com milhares de cidadãos a ganharem cerca de R$25. todas as semanas com a ajuda da Digital Real. Esta nova criptomoeda é agora amplamente utilizada e continua a ajudar milhares de pessoas.
+
+        <h2>- Qual é o princípio do investimento no Real Digital?</h2>
+        - O papel do banco na negociação com o Real Digital envolve alavancar seus recursos para executar negócios em nome dos investidores. A equipe de especialistas financeiros e os algoritmos de IA do banco analisam as tendências do mercado, identificam oportunidades potencialmente lucrativas e tomam decisões comerciais informadas. Isso garante que os investidores possam se beneficiar da experiência do banco e maximizar o retorno de seus investimentos.
+
+        <h2>- Quanto dinheiro os cidadãos podem ganhar?</h2>
+        - Os ganhos potenciais em investir no Digital Real dependem do depósito inicial feito pelo usuário. Como os algoritmos de IA geram lucros por meio de atividades de negociação, o investimento do usuário cresce exponencialmente. Em média, os usuários podem esperar que seu depósito aumente de 3 a 4 vezes, resultando potencialmente em ganhos financeiros significativos. Os utilizadores podem esperar ganhar cerca de 283.608 PTE com o seu investimento inicial. O crescimento inicial geralmente é perceptível no primeiro dia, e resultados substanciais podem ser vistos em três dias. Quanto mais dinheiro os usuários depositarem, mais dinheiro eles poderão ganhar no final.
+
+        <h2>
+          - O Digital Real é gratuito?
+        </h2>
+        - Sim, atualmente o acesso à plataforma é gratuito; no entanto, os usuários são obrigados a fazer um investimento inicial. O banco quer ajudar o maior número possível de pessoas, por isso os usuários são incentivados a participar do projeto prontamente. É importante observar que o acesso ao Digital Real pode ser limitado no futuro e os usuários podem precisar pagar mais para coDecidimos entrevistar o representante do banco para saber mais sobre a plataforma e como ela pode ser útil para o cidadão comum:ntinuar ganhando com a criptomoeda.
+
+        <h2>
+          -Como posso me tornar um membro da plataforma de negociação Digital Real?
+        </h2>
+        - Agora, as pessoas comuns podem acessar facilmente a plataforma de negociação fornecida pelo banco. O processo de registro é simples:
+        1. Preencha o formulário na plataforma.
+        2. Atenda a chamada do gerente pessoal.
+        3. Deposite o valor mínimo necessário.
+        4. Observe como a plataforma cuida de tudo para você.
+
+        <h2>
+          Vantagens de Usar a Criptomoeda
+        </h2>
+        A Digital Real foi criada para ajudar os cidadãos portugueses comuns a lucrar com o mínimo de investimento. Vejamos as principais vantagens de investir na Digital Real por meio do Banco do Brasil S.A.
+
+        <h2>
+          Confiabilidade e Responsabilidade
+        </h2>
+        O Banco do Brasil S.A. é um banco confiável e confiável que assume total responsabilidade pelos fundos dos usuários. O banco utiliza criptografia avançada e tecnologia segura para proteger as informações financeiras dos usuários, garantindo um ambiente de investimento seguro.
+
+        <h2>Acessibilidade</h2>
+        Independentemente do seu cargo ou idade, todos os residentes portugueses podem beneficiar da nova criptomoeda e registar-se facilmente no projeto. Você não precisa realizar uma pesquisa extensa, basta seguir o link fornecido.
+
+        <h2>Simplicidade</h2>
+        Uma vez feito o depósito, a plataforma Digital Real assume o processo de negociação. O banco utiliza algoritmos avançados de inteligência artificial para analisar de forma autônoma as tendências do mercado, identificar oportunidades de negociação lucrativas e executar negociações em nome dos investidores. Isso simplifica o processo de investimento para os usuários, que podem contar com a expertise da plataforma.
+
+        <h2>Riscos Zero</h2>
+        Todas as transações comerciais são feitas por inteligência artificial, garantindo uma abordagem orientada ao usuário. Isso significa que os usuários se beneficiarão de negócios lucrativos e verão seu dinheiro se multiplicar sem nenhum risco.
+        Ao considerar essas vantagens, os indivíduos podem tomar uma decisão informada e potencialmente se beneficiar do investimento em Real Digital por meio do Banco do Brasil S.A.
+
+        <h2>Depoimentos de Usuários Reais</h2>
+        Como mencionamos anteriormente, milhares de pessoas já experimentaram a plataforma de negociação Digital Real. Alguns usuários estão tão empolgados com a criptomoeda que decidiram compartilhar suas emoções com pessoas que hesitam em dar o primeiro passo.
+
+        <h3>Miguel:</h3>
+
+        "Eu costumava trabalhar duro para sustentar minha família, mas o digital real se tornou meu salvador. Depositei 59.100 PTE e esperei para ver se a notícia não mentia. E não o fizeram, recebi 236.400 PTE em três dias. Agora recebo um rendimento estável de 709.000 contos por mês e não preciso de ir para um trabalho cansativo. Finalmente, minha família e eu vivemos em abundância e posso acreditar que isso está acontecendo na vida real."
+
+        <h3>Laura:</h3>
+
+        "Trabalho como maquiadora há dois anos. O fato é que não consegui encontrar um emprego mais decente em nosso país. Mas eu ainda estava seguindo meu sonho e finalmente se tornou realidade. Agora posso aproveitar meu hobby e obter a renda principal do Real Digital. Decidi depositar R$1500, e eles multiplicaram algumas vezes! Agora eu invisto regularmente e aproveito a renda. Você não deve hesitar em investir seu dinheiro em novas tecnologias porque pode realmente ser útil hoje em dia."
+      </div>
+    </div>
+  );
+};
+
+export default Article;
---
+++ src/components/Form/Form.tsx
@@ -0,0 +1,102 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
+import React, { useState } from 'react';
+
+import './form.scss';
+
+interface FormState {
+  name: string;
+  surname: string;
+  email: string;
+  phone: string;
+}
+const Form = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    surname: '',
+    email: '',
+    phone: '',
+  });
+  const [errors, setErrors] = useState<Partial<FormState>>({});
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+
+    setFormData({ ...formData, [name]: value });
+    setErrors({ ...errors, [name]: '' });
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    let isFormValid = true;
+    const newErrors: Partial<FormState> = {};
+
+    Object.entries(formData).forEach(([key, value]) => {
+      if (value.trim() === '') {
+        newErrors[key as keyof FormState] = `${key} is required`;
+        isFormValid = false;
+      }
+
+      if ((key === 'name' || key === 'surname') && !/^[a-zA-Z]+$/.test(value)) {
+        newErrors[key] = `${key} can consist only of letter`;
+        isFormValid = false;
+      }
+
+      if (key === 'phone' && !/^[0-9]+$/.test(value)) {
+        newErrors[key] = `${key} number can only consist of numbers`;
+        isFormValid = false;
+      }
+    });
+
+    if (!isFormValid) {
+      setErrors(newErrors);
+
+      return;
+    }
+
+    alert(Object.values(formData));
+  };
+
+  return (
+    <div className="container">
+      <form className="form" onSubmit={handleSubmit}>
+        <div className="form-group">
+          <input
+            type="text"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+            placeholder="name"
+            className="form-input"
+          />
+          {errors.name && <div className="error">{errors.name}</div>}
+        </div>
+
+        <div className="form-group">
+          <input
+            type="text"
+            className="form-input"
+            name="surname"
+            value={formData.surname}
+            onChange={handleChange}
+            placeholder="surname"
+          />
+          {errors.surname && <div className="error">{errors.surname}</div>}
+        </div>
+
+        <div className="form-group">
+          <input className="form-input" type="email" placeholder="email" name="email" value={formData.email} onChange={handleChange} />
+          {errors.email && <div className="error">{errors.email}</div>}
+        </div>
+
+        <div className="form-group">
+          <input className="form-input" type="tel" placeholder="phone" name="phone" value={formData.phone} onChange={handleChange} />
+          {errors.phone && <div className="error">{errors.phone}</div>}
+        </div>
+
+        <button type="submit" className="submit">Send</button>
+      </form>
+    </div>
+  );
+};
+
+export default Form;
